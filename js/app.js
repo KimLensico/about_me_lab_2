98150669950a9d21f9a0686448d152723a8df007
KIMBERLY LENSICO <KIMBERLYLENSICO@OUTLOOK.COM>
2020-09-03 18:04:31 -0700

    let it live

--- js/app.js
+++ js/app.js
@@ -1,51 +1,54 @@
-'use strict';
-
-var correctanswers= 0;
+'use strict'
+ 
+var correctAnswer = 0;
 
 console.log("js is loaded");
-
+ 
 var ready = confirm("Would you like to be friends?");
-
+ 
 console.log(ready);
-
+ 
 //'let's ask some questions
-
-if (ready === true) {
-    var answeredProperly = false;
-    while (answeredProperly === false) {
-        var answer1 = prompt("Do you think I would want to be your friend?:");
-        console.log(answer1.toLowerCase());
-        if (answer1.toLowerCase() === "yes") {
-            answeredProperly = true;
-            alert("That's great, I think You're hella cool!");
+function friends() {
+    if (ready === true) {
+        var answeredProperly = false;
+        while (answeredProperly === false) {
+            var answer1 = prompt("Do you think I would want to be your friend?:");
             console.log(answer1.toLowerCase());
-        } else if (answer1 === "no") {
-            answeredProperly = true;
-            alert("That's unfortunate, you'll be missing out.");
-            console.log(answer1.toLowerCase());
-        } else {
-            alert("You need to answer Yes or No");
-            console.log(answer1.toLowerCase());
+            if (answer1.toLowerCase() === "yes") {
+                answeredProperly = true;
+                alert("That's great, I think You're hella cool!");
+                console.log(answer1.toLowerCase());
+            } else if (answer1 === "no") {
+                answeredProperly = true;
+                alert("That's unfortunate, you'll be missing out.");
+                console.log(answer1.toLowerCase());
+            } else {
+                alert("You need to answer Yes or No");
+                console.log(answer1.toLowerCase());
+            }
         }
     }
+}
+ 
+function hike () {
     var hiking = prompt("Do you like hiking?");
     while (hiking) {
         if (hiking.toLowerCase() === "yes") {
             alert("Sweet me too!");
             var joinMe = prompt("I know a place with a great view, would you like to check it out?");
             if (joinMe.toLowerCase() === 'yes') {
-                alert("Lets' go!")
+                alert("Lets' go!");
             }
             console.log(hiking.toLowerCase());
         } else if (hiking.toLowerCase() === "no") {
-            var no = prompt("Have you ever gone hiking to know that you don't like it?")
+            var no = prompt("Have you ever gone hiking to know that you don't like it?");
             if (no.toLowerCase() === 'yes') {
             var yes = prompt("Would you like to try it again with me sometime?");
-            if (yes.toLowerCase() === 'yes') {
-            var nameLocation = prompt("Where would you like to go?");
+            } if (yes.toLowerCase() === 'yes') {
+                var nameLocation = prompt("Where would you like to go?");
                 alert("We're going to " + nameLocation.toLowerCase() + " then!");
                 console.log(nameLocation.toLowerCase());
-            }
             } else if (no.toLowerCase() === "no") {
                 alert("Let's try it sometime then!");
                 console.log(no.toLowerCase());
@@ -53,9 +56,13 @@ if (ready === true) {
                 alert("You need to answer yes or no");
                 console.log(no.toLowerCase());
             }
-        } 
-        break
-    }
+        } else {
+            break;
+        }
+    }      
+}
+ 
+function choco() {
     var cookies = prompt("Do you like cookies?");
         if (cookies.toLowerCase() === "yes") {
             alert("I have a lot, let's eat them all right now");
@@ -68,40 +75,54 @@ if (ready === true) {
             console.log(cookies.toLowerCase());
         }
 }
-
+ 
+ 
 //ask user to guess my age
+function grow() {
     var answer = 28
     for (var guess = 0; guess < 4; guess++) {
         var age = prompt("How old do you think I am?");
         if (parseInt(age) === answer) {
             alert ("Correct!");
-            correctanswers++ 
-        break;
+            correctAnswer++; 
+            break;
         } else if (parseInt(age) < answer) {
             alert ("your guess is too low");
         } else if (parseInt(age) > answer) {
             alert ("your guess is too high");
-            
         }
-    }   
+    }
+}   
+ 
+ 
 //ask user to guess what states I have lived in
-    var answers = ["new york" , "california" , "nevada"];
-
+function born(){
+    var answers = ["new york" , "california" , "nevada"]; 
+ 
     for (var guess = 0; guess < 6; guess++) {
         var locate = prompt("Can you guess at least one state that I have lived in?");
         console.log(locate);
-        if (answers.includes(locate.toLowerCase())) {
-            correctanswers++
+        if (locate.toLowerCase() === answers[guess]) {
+            correctAnswer++;
             alert ("YES! You got it!");
-        break;
-        
-        // } else if (guess === 5 && !answers.includes(locate.toLowerCase())) {
+            break;
+        } else if (guess === 5 && answers[guess](locate.toLowerCase())) {
             alert ("The correct answers are: new york, california, nevada!");
-        }
-        
-        
-        else {
+        } else {
             alert ("Sorry, keep guessing!");
         }
     }
-    alert ("You guessed " + correctanswers + " correctly");
+}
+ 
+friends();
+ 
+hike();
+ 
+choco();
+ 
+grow();
+ 
+born();
+ 
+alert ("You guessed " + correctAnswer + " correctly");
+
